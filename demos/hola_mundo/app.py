28ad1e142d6640d00c1db3bf98a46e30f625619d
angelzoyate <1725110545@utectulancingo.edu.mx>
2026-05-29 16:22:50 +0000

    ADD: estructura de carpetas demos/hola_mundo

--- demos/hola_mundo/app.py
+++ demos/hola_mundo/app.py
@@ -6,7 +6,7 @@ urls = (
 
 class Index:
     def GET(self):
-        return "¡Hola Mundo!"
+        return "Esta en la URL 1"
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
